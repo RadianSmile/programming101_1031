4f28430dd709b73fdd39c3d09ca6717de56ac2fd
Radian <craziestdavid@gmail.com>
2014-08-09 17:22:23 +0800

    define

--- ace/src-noconflict/mode-processing.js
+++ ace/src-noconflict/mode-processing.js
@@ -56,7 +56,7 @@ oop.inherits(Mode, TextMode);
 exports.Mode = Mode;
 });
 
-define('ace/mode/processing_highlight_rules', ['require', 'exports', 'module' , 'ace/lib/oop', 'ace/mode/text_highlight_rules'], function(require, exports, module) {
+ace.define('ace/mode/processing_highlight_rules', ['require', 'exports', 'module' , 'ace/lib/oop', 'ace/mode/text_highlight_rules'], function(require, exports, module) {
 
 
 var oop = require("../lib/oop");
