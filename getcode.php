503297867bfb9ea4988492b3452203878a1a0def
Radian <craziestdavid@gmail.com>
2014-08-26 12:34:56 +0800

    ff

--- getcode.php
+++ getcode.php
@@ -1,1 +1,35 @@
-<?php echo file_get_contents($_POST['url']); ?>
+<?php
+
+$url ;
+if (isset($_POST['url']) )
+{
+	$url = $_POST['url'];
+}else {
+	$url = "http://radiansmile.github.io/CodeEDU/final_11/play.html";
+}
+
+
+//$filename = substr(strrchr($url, "/"), 1);
+echo strripos($url, "/") . "<br>" ;
+echo strlen($url) . "<br>";
+echo substr($url, 0 , strripos($url, "/") ) . "/";
+
+//echo $filename = substr ()
+
+$doc = new DomDocument;
+// We need to validate our document before refering to the id
+$doc->validateOnParse = true;
+libxml_use_internal_errors(true);
+$doc->loadHtml(file_get_contents($url));
+libxml_clear_errors();
+
+$a = $doc->getElementById("pde");
+$b = $url . $a->getAttribute("data-processing-sources");
+
+//echo file_get_contents($b);
+
+
+/**Rn** http://stackoverflow.com/questions/5045598/getting-elements-of-a-div-from-another-page-php  */
+?>
+
+
--- js/all.js
+++ js/all.js
@@ -1,23 +1,25 @@
 // JavaScript Document
 
-window.fbAsyncInit = function() {
-  Parse.initialize("9eo5r1mHWoIPSTCzmrpdKa3lcHPjySx4y5D6q8Nq", "R8SWwYxpJcy73ogQKuSD43y7FigrlDGjBLcy1lzC");	
-	Parse.FacebookUtils.init({
-    appId      : '1452756891666119', // Facebook App ID
-    cookie     : true, // enable cookies to allow Parse to access the session
-    xfbml      : true,  // parse XFBML
-    version    : 'v1.0'
-  });
-	
-	/*FB.getLoginStatus(function(response) {
-		statusChangeCallback(response);
-	});*/
-  };
+      window.fbAsyncInit = function() {
+         Parse.initialize("9eo5r1mHWoIPSTCzmrpdKa3lcHPjySx4y5D6q8Nq", "R8SWwYxpJcy73ogQKuSD43y7FigrlDGjBLcy1lzC");	
+					Parse.FacebookUtils.init({
+    			 appId      : '1452756891666119', // Facebook App ID
+          xfbml      : true,
+					 cookie     : true, // enable cookies to allow Parse to access the session
+  				  version    : 'v1.0'
+        });
+      };
 
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/zh_TW/all.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/all.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+
+
+
+
+
+
