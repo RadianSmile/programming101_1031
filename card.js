00c08a43f7f091d3e57c6d4f506bb6512d9040a5
s914211 <cchccy1623@gmail.com>
2014-08-31 22:09:56 +0800

    Update card.js

--- card.js
+++ card.js
@@ -2,8 +2,8 @@
 $(document).ready(function(){
     Parse.initialize("9eo5r1mHWoIPSTCzmrpdKa3lcHPjySx4y5D6q8Nq", "R8SWwYxpJcy73ogQKuSD43y7FigrlDGjBLcy1lzC");
     var current_user = Parse.User.current();
-    console.log(current_user);
     if(current_user){
+        console.log(current_user);
         var ownCard = Parse.Object.extend('Owncard');
         var query = new Parse.Query(ownCard);
         query.include('Card_info');
