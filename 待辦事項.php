74118486cc3218ef453ce740fca90b12e21c7efe
Radian <craziestdavid@gmail.com>
2014-08-31 09:27:18 +0800

    f

---
+++ 待辦事項.php
@@ -0,0 +1,17 @@
+<?php
+
+
+//修正註冊頁面的Bug
+
+// iframe cors resize
+
+"http://davidjbradshaw.github.io/iframe-resizer/"
+
+//註冊頁面要防堵註冊過的人 // 要變成 禁止 public update
+
+// 評分那邊，要加上Bug
+// 加上，時間防堵措施
+
+
+
+?>
