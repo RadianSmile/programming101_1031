8209aac0364bb00b8da446de86c0de03be07c591
Tommy <cchccy1623@gmail.com>
2014-08-31 19:34:39 +0800

    try card

--- card.js
+++ card.js
@@ -38,7 +38,7 @@ $(document).ready(function(){
     }
     else{
     	alert("Please Login!");
-    	window.location.assign("fblogin.html");
+    	window.location.assign("http://radiansmile.github.io/fblogin.html");
     }
 });
 
