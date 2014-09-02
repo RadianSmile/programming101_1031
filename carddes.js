771bad8056a5c65f5f6b6c01cf84b6e64926baaa
s914211 <cchccy1623@gmail.com>
2014-09-02 22:51:37 +0800

    try card

--- carddes.js
+++ carddes.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     Parse.initialize("9eo5r1mHWoIPSTCzmrpdKa3lcHPjySx4y5D6q8Nq", "R8SWwYxpJcy73ogQKuSD43y7FigrlDGjBLcy1lzC");
     var current_user = Parse.User.current();
     if(current_user){
-        var owncardid = localStorage.getItem('owncardid');
+        var owncardid = localStorage.getItem('owncardId');
         console.log(owncardid);
         var ownCard = Parse.Object.extend('Owncard');
         var query = new Parse.Query(ownCard);
