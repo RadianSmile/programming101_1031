9c4d5f07ef50cabf67ccdee957ee7f30473d4768
Tommy <cchccy1623@gmail.com>
2014-09-02 17:05:24 +0800

    try card

--- card.js
+++ card.js
@@ -53,7 +53,7 @@ $(document).ready(function(){
 
 function getElementStringByowncard(name, imagesrc, id){
     var s0 = "<h2>" + name + "</h2>";
-    var s1 = "<img' id='"+id+"' src='" + imagesrc + "'>";
+    var s1 = "<img id='"+id+"' src='" + imagesrc + "'>";
     var s = "<div class='card'>" + s0 + s1 + "</div>";
 
     return s;
