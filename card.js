c13ee0b8bba4f40287b7ffe422da297bbc3a988e
Tommy <cchccy1623@gmail.com>
2014-09-02 13:29:42 +0800

    try card

--- card.js
+++ card.js
@@ -53,7 +53,7 @@ $(document).ready(function(){
 
 function getElementStringByowncard(name, imagesrc, i){
     var s0 = "<h2>" + name + "</h2>";
-    var s1 = "<a href= usecard.html><img class = 'owncard'"+'id=card'+i +" src='" + imagesrc + "'</a>";
+    var s1 = "<a href= carddes.html><img class = 'owncard'"+'id=card'+i +" src='" + imagesrc + "'</a>";
     var s = "<div class='card'>" + s0 + s1 + "</div>";
 
     return s;
