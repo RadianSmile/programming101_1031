d2dffa1a58d7466e12315acfe5200588d6c65d07
Tommy <cchccy1623@gmail.com>
2014-09-07 15:09:35 +0800

    try card

--- cardrecord.js
+++ cardrecord.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     Parse.initialize("9eo5r1mHWoIPSTCzmrpdKa3lcHPjySx4y5D6q8Nq", "R8SWwYxpJcy73ogQKuSD43y7FigrlDGjBLcy1lzC");
     var current_user = Parse.User.current();
     if(current_user){
+        //Card use
         var notification = "";
         var notif = Parse.Object.extend("Card_record");
         var query = new Parse.Query(notif);
@@ -23,7 +24,7 @@ $(document).ready(function(){
                 console.log(error.toString());
             }
         });
-
+        //Card get
         var notification1 = "";
         var notif1 = Parse.Object.extend("Card_record");
         var query1 = new Parse.Query(notif1);
@@ -39,6 +40,29 @@ $(document).ready(function(){
                     var strings = "<div class = 'cardnotification'>" + notification1 + "</div>";
                     $('div.notificationbox').append(strings);
                     notification1 = "";
+                }
+            },
+            error:function(error){
+                console.log(error.toString());
+            }
+        });
+
+        //Card draw
+        var notification2 = "";
+        var notif2 = Parse.Object.extend("Card_record");
+        var query2 = new Parse.Query(notif2);
+        query2.equalTo('type', "draw");
+        query2.include('Card_info');
+        query2.include('User');
+        query2.ascending('date');
+        query2.find({
+            success:function(data){
+                for(var i = 0; i<data.length; i++){
+                    var s = drawRecord(data[i]);
+                    notification2 += s;
+                    var strings = "<div class = 'cardnotification'>" + notification2 + "</div>";
+                    $('div.notificationbox').append(strings);
+                    notification2 = "";
                 }
             },
             error:function(error){
@@ -76,6 +100,16 @@ function getRecord(data){
     var s = "";
         if(userId == Parse.User.current().id){
             s = "<h2>你抽到了"+ cardName + "。</h2>";
+        }
+    return s;
+}
+
+function drawRecord(data){
+    var userId = data.get('user').id;
+    
+    var s = "";
+        if(userId == Parse.User.current().id){
+            s = "<h2>你有一次抽卡機會！</h2>";
         }
     return s;
 }
