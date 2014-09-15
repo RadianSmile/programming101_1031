c402eabecf59256d63c149aaffcd33372a38d289
s914211 <cchccy1623@gmail.com>
2014-09-15 22:32:41 +0800

    try card

--- notification.js
+++ notification.js
@@ -52,7 +52,6 @@ $(document).ready(function(){
         query.descending('createdAt');
         query.find({
             success:function(data){
-                console.log(data);
                 for(var i = 0; i<data.length; i++){
                     data[i].set('isNotif', true);
                     data[i].save(null,{
