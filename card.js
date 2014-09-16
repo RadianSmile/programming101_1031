ae3fe0cad0ed7693f9c3fcf00db2d621ddda88e2
Tommy <cchccy1623@gmail.com>
2014-09-16 16:58:41 +0800

    card drawing tooltip

--- card.js
+++ card.js
@@ -48,8 +48,12 @@ $(document).ready(function(){
                     for(var i = 0; i<data.length; i++){
                         var card = data[i].get('Card_info');
                         if(card == undefined){
-                            var backcard = "<div class='card-box col-md-2'><a href='card_drawing.html' data-toggle='tooltip' data-placement='top' title='你有一張抽卡機會'><img id='backcard' src='img/choosecard/back.png' alt='alternative text' title='你有一張抽卡機會'></a></div>"; // Rn
+                            var backcard = "<div class='card-box col-md-2'><a href='card_drawing.html' data-toggle='tooltip' title='你有一張抽卡機會'><img id='backcard' src='img/choosecard/back.png' alt='alternative text' title='你有一張抽卡機會'></a></div>"; // Rn
                             $('div#OwnCardData').append(backcard);
+                            $('a[data-toggle="tooltip"]').tooltip({
+                                animated: 'fade',
+                                placement: 'top',
+                            });
                         }
                         else{
                             var s = getElementStringByowncard(card.get('imagesrc'), data[i].id);
