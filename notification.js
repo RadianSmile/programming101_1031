afd01c776d9d96410c3bf0689a70a41c2bf90a75
Tommy <cchccy1623@gmail.com>
2014-09-17 19:35:54 +0800

    change notification icon

--- notification.js
+++ notification.js
@@ -116,16 +116,16 @@ function useRecord(data){
         if(userId == Parse.User.current().id){
             if(targetId == Parse.User.current().id){
                 s = "你對自己使用了" + cardName + "。";
-                container = "<div class = 'time-gray-color'>"+createTime+"</div><span class = 'glyphicon glyphicon-thumbs-down' style = 'white-space: nowrap;'>"+ s +"</span></div>";
+                container = "<div class = 'time-gray-color'>"+createTime+"</div><span class = 'glyphicon glyphicon-bullhorn' style = 'white-space: nowrap;'>"+ s +"</span></div>";
             }
             else{
                 s = "你對" + targetName + "使用了" + cardName + "。";
-                container = "<div class = 'time-gray-color'>"+createTime+"</div><span class = 'glyphicon glyphicon-thumbs-down' style = 'white-space: nowrap;'>"+ s +"</span></div>";
+                container = "<div class = 'time-gray-color'>"+createTime+"</div><span class = 'glyphicon glyphicon-bullhorn' style = 'white-space: nowrap;'>"+ s +"</span></div>";
             }
         }
         else if(targetId == Parse.User.current().id){
             s = userName+"對你使用了" + cardName + "。";
-            container = "<div class = 'time-gray-color'>"+createTime+"</div><span class = 'glyphicon glyphicon-thumbs-down' style = 'white-space: nowrap;'>"+ s +"</span></div>";
+            container = "<div class = 'time-gray-color'>"+createTime+"</div><span class = 'glyphicon glyphicon-bullhorn' style = 'white-space: nowrap;'>"+ s +"</span></div>";
         }
     return container;
 }
@@ -139,7 +139,7 @@ function getRecord(data){
     var s = "";
         if(userId == Parse.User.current().id){
             s = "你抽到了"+ cardName + "。";
-            container = "<div class = 'time-gray-color'>"+createTime+"</div><span class = 'glyphicon glyphicon-thumbs-down' style = 'white-space: nowrap;'>"+ s +"</span></div>";
+            container = "<div class = 'time-gray-color'>"+createTime+"</div><span class = 'glyphicon glyphicon-bullhorn' style = 'white-space: nowrap;'>"+ s +"</span></div>";
         }
     return container;
 }
@@ -155,11 +155,11 @@ function eventRecord(data, data1){
     var s = "";
     if(hp >=0){
         s = "因為" + eventdes + "，所以造成你的XP增加" + xp +"、你的HP增加" + hp + "、你的抽卡機會增加" + draw + "次。";
-        container = "<div class = 'time-gray-color'>"+createTime+"</div><span class = 'glyphicon glyphicon-thumbs-down' style = 'white-space: nowrap;'>"+ s +"</span></div>";
+        container = "<div class = 'time-gray-color'>"+createTime+"</div><span class = 'glyphicon glyphicon-bullhorn' style = 'white-space: nowrap;'>"+ s +"</span></div>";
     }
     else{
         s = "因為" + eventdes + "，所以造成你的XP增加" + xp +"、你的HP被" + hp + "、你的抽卡機會增加" + draw + "次。";
-        container = "<div class = 'time-gray-color'>"+createTime+"</div><span class = 'glyphicon glyphicon-thumbs-down' style = 'white-space: nowrap;'>"+ s +"</span></div>";
+        container = "<div class = 'time-gray-color'>"+createTime+"</div><span class = 'glyphicon glyphicon-bullhorn' style = 'white-space: nowrap;'>"+ s +"</span></div>";
     }
     return container;
 }
